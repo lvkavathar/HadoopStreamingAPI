5865606fd7ce5b3fd3ffe59ce0070c3d05b2b224
lingalvy <lingalvy@mail.uc.edu>
2016-12-02 23:52:28 -0500

    Update reducer.py

--- mr_ex/reducer.py
+++ mr_ex/reducer.py
@@ -2,23 +2,29 @@
 
 from operator import itemgetter
 import sys
+
+#initialing the variables
 word = None
-current_count = 0
-current_word = None
+cur_count = 0
+cur_word = None
 
 for line in sys.stdin:
+        #stripping the whitespaces
         line=line.strip()
+        #spliting the line to word,count
         word,count = line.split('\t')
+        #converting count variable from string to int
         try:
           count = int(count)
         except ValueError:
           continue
-        if current_word == word:
-                current_count += count
+        #AS mapreduce framework sorts the output of mapper based on key we are going to use it in counting
+        if cur_word == word:
+                cur_count += count
         else:
-          if current_word:
-                print "%s\t%s" %(current_word,current_count)
-          current_count = count
-          current_word = word
-if current_word == word:
-        print "%s\t%s" %(current_word,current_count)
+          if cur_word:
+                print "%s\t%s" %(cur_word,cur_count)
+          cur_count = count
+          cur_word = word
+if cur_word == word:
+        print "%s\t%s" %(cur_word,cur_count)
