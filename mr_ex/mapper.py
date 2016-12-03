4d6e0c64c95b421f965d74057e33252bc36303d7
lingalvy <lingalvy@mail.uc.edu>
2016-12-02 23:47:15 -0500

    Update mapper.py

--- mr_ex/mapper.py
+++ mr_ex/mapper.py
@@ -3,10 +3,14 @@
 import sys
 import re
 for linenum,line in enumerate(sys.stdin):
+        #removing header from the file
         if linenum != 0 :
                 count = 0
+                #striping whitespaces in a line
                 line = line.strip()
+                #replacing the content in "" by none
                 line = re.sub(r'".*"', '', line)
+                #splitting the line in to words seperated by comma
                 words=line.split(',')
                 for word in words:
                         count = count + 1
