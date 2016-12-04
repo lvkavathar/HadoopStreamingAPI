e3ff9f6623e76f9621df0cb26a39dadb6e0a9647
lingalvy <lingalvy@mail.uc.edu>
2016-12-04 08:07:26 -0500

    Update mapper.py

--- mr_ex/mapper.py
+++ mr_ex/mapper.py
@@ -10,9 +10,10 @@ for line in sys.stdin:
                 line = re.sub(r'".*"', '', line)
                 #splitting the line in to words seperated by comma
                 words=line.split(',')
-                #stripping header from the file
+                #skipping header from the file
                 if words[0] != "DATE" :
                         for word in words:
                                 count = count + 1
                                 if count >= 25 and word != "" :
                                         print '%s\t%s' %(word,1)
+
